0ee1901c7bae4a9828c1574fcf3139b3d83e2005
Sheizer-debug <shreyansaha18@gamil.com>
2023-04-02 18:37:58 +0530

    Input Field Generated

--- src/InputField.js
+++ src/InputField.js
@@ -1,10 +1,10 @@
-import  './Input.css'
 import Tooltip from '@mui/material/Tooltip';
 import InfoIcon from '@mui/icons-material/Info';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
-export default function Input({currentField,formData,handleChange,checked})
+import  './InputField.css';
+export default function InputField ({currentField,formData,handleChange,checked})
 {
     let required=currentField.validate.required,key=currentField.jsonKey;
     console.log('Inside Input',required,currentField,key,formData,handleChange,checked)
@@ -23,7 +23,7 @@ export default function Input({currentField,formData,handleChange,checked})
                 <TextField required id={currentField.label} value={formData[key]} onChange={(event)=>handleChange(event,key)} sx={{width:"17.2rem", height:"100%",backgroundColor:"#F0F7FF",border: "1px solid aliceblue" }} variant="outlined" />
             </Stack>
         : checked ?
-            <Stack sx={{padding:"2px",marginBottom:"6px"}}direction="row" justifyContent="space-around" spacing={20}>
+                <Stack sx={{padding:"2px",marginBottom:"6px"}}direction="row" justifyContent="space-around" spacing={20}>
                 <label htmlFor={currentField.label} className="col-sm-2 col-form-label">{currentField.label}<span className="required">*</span>
                     {currentField.description !== '' && 
                     <Tooltip title={currentField.description}>
@@ -33,7 +33,7 @@ export default function Input({currentField,formData,handleChange,checked})
                     </Tooltip>
                     }
                 </label>
-                <TextField id={currentField.label}  sx={{width:"17.2rem", height:"100%",backgroundColor:"#F0F7FF",border: "1px solid aliceblue" }} variant="outlined" />
+                <TextField id={currentField.label} value={formData[key]} onChange={(event)=>handleChange(event,key)} sx={{width:"17.2rem", height:"100%",backgroundColor:"#F0F7FF",border: "1px solid aliceblue" }} variant="outlined" />
             </Stack>
         :null
     );
---
+++ src/SelectField.js
@@ -0,0 +1,56 @@
+import Stack from '@mui/material/Stack';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+export default function SelectField({currentField,formData,handleChange,checked})
+{
+    let required=currentField.validate.required,key=currentField.jsonKey;
+    console.log('Inside Select',required,currentField,key,formData,handleChange,checked)
+    return(
+        required ?
+                  <Stack sx={{padding:"2px"}}direction="row" justifyContent="space-around" spacing={20}>
+                    <label htmlFor={currentField.label} className="col-sm-2 col-form-label">{currentField.label}<span className="required">*</span></label>
+                    <FormControl sx={{ width:"17.2rem",backgroundColor:"#F0F7FF" }}>
+                      <Select displayEmpty id={currentField.label} inputProps={{ 'aria-label': 'Without label' }} value={formData[key]} onChange={(event)=>handleChange(event,key)}>
+                        {currentField.validate.options.map((option) => (
+                          <MenuItem value={option.label}>{option.value}</MenuItem> 
+                        ))}
+                      </Select>
+                   </FormControl>
+                 </Stack>
+        : checked ?
+                    <Stack sx={{padding:"2px"}}direction="row" justifyContent="space-around" spacing={20}>
+                      <label htmlFor={currentField.label} className="col-sm-2 col-form-label">{currentField.label}</label>
+                      <FormControl sx={{ width:"17.2rem",backgroundColor:"#F0F7FF" }}>
+                        <Select id={currentField.label} displayEmpty inputProps={{ 'aria-label': 'Without label' }} value={formData[key]} onChange={(event)=>handleChange(event,key)}>
+                          {currentField.validate.options.map((option) => (
+                            <MenuItem value={option.label}>{option.value}</MenuItem> 
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </Stack>
+        :null
+    );
+}
+                // if (currentField.validate.required) {
+                //   fieldElement = (
+                //     <Stack sx={{padding:"2px"}}direction="row" justifyContent="space-around" spacing={20}>
+                //       <label for="staticEmail" className="col-sm-2 col-form-label">{currentField.label}</label>
+                //       <TextField required id="filled-basic" label="Filled" sx={{width:"17.2rem", height:"1rem",backgroundColor:"#F0F7FF",border: "1px solid aliceblue" }} variant="filled" />
+                //     </Stack>
+                //   );
+                // } else if (props.checked) {
+                //   let key=currentField.jsonKey;
+                //   fieldElement = (
+                //     <Stack sx={{padding:"2px"}}direction="row" justifyContent="space-around" spacing={20}>
+                //       <label for="staticEmail" className="col-sm-2 col-form-label">{currentField.label}</label>
+                //       <FormControl sx={{ width:"17.2rem",backgroundColor:"#F0F7FF" }}>
+                //         <Select displayEmpty inputProps={{ 'aria-label': 'Without label' }} value={formData[key]} onChange={(event)=>props.handleChange(event,key)}>
+                //           {currentField.validate.options.map((option) => (
+                //             <MenuItem value={option.label}>{option.value}</MenuItem> 
+                //           ))}
+                //         </Select>
+                //       </FormControl>
+                //     </Stack>
+                //   );
+                // }   
